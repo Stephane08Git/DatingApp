cc3cf214b7492935d8adef570ba4349c74178885
toffoletti-stephane@hotmail.fr <toffoletti-stephane@hotmail.fr>
2021-02-03 15:45:12 +0100

    End of section 2

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+// using Microsoft.Extensions.Logging;
+using System.Security.Cryptography;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot"
+        };
+
+        // private readonly ILogger<WeatherForecastController> _logger;
+
+        // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Summaries.Select((element, index) => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = RandomNumberGenerator.GetInt32(-20, 55),
+                Summary = Summaries[index]
+            })
+            .ToArray();
+        }
+    }
+}
